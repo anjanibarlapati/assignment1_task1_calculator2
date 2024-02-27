f4e2c6fb2a07e90fd264013c1743abfc6b107eb3
Admin <admin@Admins-MacBook-Air.local>
2024-02-27 11:05:11 +0530

    main.java in master

---
+++ main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class main
+{
+      
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int a,b;
+        a=sc.nextInt();
+        b=sc.nextInt();
+        int add=addition(a,b);
+        int sub=diff(a,b);
+        System.out.println("Sum is:"+add);
+        System.out.println("Diff is:"+sub);
+    }
+}
