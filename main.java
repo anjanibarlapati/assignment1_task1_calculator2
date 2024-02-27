54e11d6e30fa71ab143d98b2c30ef3bf54d87acf
Admin <admin@Admins-MacBook-Air.local>
2024-02-27 11:13:39 +0530

    subtraction is added

--- main.java
+++ main.java
@@ -1,7 +1,14 @@
 import java.util.Scanner;
 class main
 {
-      
+
+
+    int diff(int a,int b)
+    {
+        return a-b;
+    }
+
+
     public static void main(String[] args)
     {
         Scanner sc=new Scanner(System.in);
