c863d5c50d559ab84aaa512cb90c83255e5fcc5c
Admin <admin@Admins-MacBook-Air.local>
2024-02-27 11:09:23 +0530

    addition feature is added

--- main.java
+++ main.java
@@ -1,7 +1,12 @@
 import java.util.Scanner;
 class main
 {
-      
+
+    int addition(int a,int b)
+    {
+        return a+b;
+    }
+  
     public static void main(String[] args)
     {
         Scanner sc=new Scanner(System.in);
